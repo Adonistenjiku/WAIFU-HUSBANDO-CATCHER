3419f886982a420c2521b4ddb665d2b5adeb39b7
NB_Adonis_TENJIKU <sutradharapurba006@gmail.com>
2025-01-25 14:26:10 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "5886080930"
+    sudo_users = "5886080930", "7972319522","5268691896"
+    GROUP_ID = -1002137208192
+    TOKEN = "6978354290:AAF-j6QEGYRPd-uwy3oE4mF2xjPuUIdrkPQ"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    PHOTO_URL = ["https://unitedcamps.in/Images/file_11559.jpg", "https://unitedcamps.in/Images/file_11413.jpg"]
+    SUPPORT_CHAT = "https://t.me/kill_your_HW_Group"
+    UPDATE_CHAT = "https://t.me/kill_your_HW_Channel"
+    BOT_USERNAME = "@Kill_your_Husbando_bot"
+    CHARA_CHANNEL_ID = "-1002137208192"
+    api_id = 25504446
+    api_hash = "47db27cde56c3e4690e244e6de10f919"
 
     
 class Production(Config):
