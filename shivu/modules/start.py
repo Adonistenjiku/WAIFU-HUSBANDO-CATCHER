9f3500ea4912224cf08b5086e2d3926c803932da
NB_Adonis_TENJIKU <sutradharapurba006@gmail.com>
2025-02-26 19:34:34 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -36,15 +36,15 @@ async def start(update: Update, context: CallbackContext) -> None:
         caption = f"""
         ***Heyyyy...***
 
-***I am An Open Source Character killer Bot...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /kill to.. Collect that Characters in Your Collection.. and see Collection by using /kHarem... So add in Your groups and Collect Your harem***
+***I am An Open Source Character ⍟ killer Bot ⍟...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /kill to.. Collect that Characters in Your Collection.. and see Collection by using /kHarem... So add in Your groups and Collect Your harem***
         """
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
-            [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            [InlineKeyboardButton("⍟ ADD ME ⍟", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("⍟ SUPPORT ⍟", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("⍟ UPDATES ⍟", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("⍟ HELP ⍟", callback_data='help')],
+            [InlineKeyboardButton("⍟ SOURCE ⍟", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         photo_url = random.choice(PHOTO_URL)
@@ -54,11 +54,11 @@ async def start(update: Update, context: CallbackContext) -> None:
     else:
         photo_url = random.choice(PHOTO_URL)
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
-            [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            [InlineKeyboardButton("⍟ ADD ME ⍟", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("⍟ SUPPORT ⍟", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("⍟ UPDATES ⍟", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("⍟ HELP ⍟", callback_data='help')],
+            [InlineKeyboardButton("⍟ SOURCE ⍟", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
         ]
         
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -97,11 +97,11 @@ async def button(update: Update, context: CallbackContext) -> None:
 
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
-            [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            [InlineKeyboardButton("⍟ ADD ME ⍟", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("⍟ SUPPORT ⍟", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("⍟ UPDATES ⍟", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("⍟ HELP ⍟", callback_data='help')],
+            [InlineKeyboardButton("⍟ SOURCE ⍟", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
